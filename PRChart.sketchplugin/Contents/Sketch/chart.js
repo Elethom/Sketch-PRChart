8d5e7b984330504d66ed18f11993f4c8b5b74da7
Elethom Hunter <elethomhunter@gmail.com>
2020-11-18 06:21:19 +0800

    Refactor with modern syntax and to be compatible with latest Sketch version

--- PRChart.sketchplugin/Contents/Sketch/chart.js
+++ PRChart.sketchplugin/Contents/Sketch/chart.js
@@ -1,187 +1,252 @@
-var makeBarChart = function(context) {
-    var dialog
-    var createUI = function() {
-        dialog = COSAlertWindow.new()
-        dialog.setMessageText('Make Bar Chart')
-        dialog.addTextLabelWithValue('width')
-        dialog.addTextFieldWithValue('600')
-        dialog.addTextLabelWithValue('height')
-        dialog.addTextFieldWithValue('200')
-        dialog.addTextLabelWithValue('max value')
-        dialog.addTextFieldWithValue('100')
-        dialog.addTextLabelWithValue('values (divided by ",")')
-        dialog.addTextFieldWithValue('')
-        dialog.addTextLabelWithValue('column width (optional)')
-        dialog.addTextFieldWithValue('')
-        dialog.addButtonWithTitle('OK')
-        dialog.addButtonWithTitle('Cancel')
-        
-        dialog.alert().window().setInitialFirstResponder(dialog.viewAtIndex(1))
-        dialog.viewAtIndex(1).setNextKeyView(dialog.viewAtIndex(3))
-        dialog.viewAtIndex(3).setNextKeyView(dialog.viewAtIndex(5))
-        dialog.viewAtIndex(5).setNextKeyView(dialog.viewAtIndex(7))
-        dialog.viewAtIndex(7).setNextKeyView(dialog.viewAtIndex(9))
-        dialog.viewAtIndex(9).setNextKeyView(dialog.viewAtIndex(1))
-        
-        return dialog.runModal()
-    }
-    var process = function() {
-        var width = dialog.viewAtIndex(1).stringValue()
-        var height = dialog.viewAtIndex(3).stringValue()
-        var max = dialog.viewAtIndex(5).stringValue()
-        var values = dialog.viewAtIndex(7).stringValue().split(',')
-        var columnWidth = dialog.viewAtIndex(9).stringValue()
-        var count = values.length
-        
-        if (width.length() == 0 ||
-            height.length() == 0 ||
-            max.length() == 0 ||
-            values.length == 0) {
-            return
-        }
-        if (columnWidth == 0 ||
-            columnWidth.length() == 0) {
-            columnWidth = width / count
-        }
-        
-        for (var i = 0; i < count; i++) {
-            var x = width / count * i + (width / count - columnWidth) * 0.5
-            var y = (max - values[i]) / max * height
-            var w = columnWidth
-            var h = height - y
-        
-            var sketch = context.api()
-            var rect = MSShapeGroup.shapeWithRect(new sketch.Rectangle(x,y,w,h).asCGRect())
-            var fill = rect.style().addStylePartOfType(0)
-            fill.setColor(MSImmutableColor.colorWithIntegerRed_green_blue_alpha(0,0,0,51).newMutableCounterpart())
-            rect.setName('Bar')
-            context.selection[0].addLayers([rect])
-        }
-    }
-    if (createUI() == 1000) {
-        process()
-    }
+const { Style, Rectangle, Shape, ShapePath } = require('sketch/dom');
+
+const defaultValues = {
+  width: 600,
+  height: 300
 }
 
-var makeLineChart = function(context) {
-    var dialog
-    var createUI = function() {
-        dialog = COSAlertWindow.new()
-        dialog.setMessageText('Make Line Chart')
-        dialog.addTextLabelWithValue('width')
-        dialog.addTextFieldWithValue('600')
-        dialog.addTextLabelWithValue('height')
-        dialog.addTextFieldWithValue('200')
-        dialog.addTextLabelWithValue('max value')
-        dialog.addTextFieldWithValue('100')
-        dialog.addTextLabelWithValue('values (divided by ",")')
-        dialog.addTextFieldWithValue('')
-        dialog.addButtonWithTitle('OK')
-        dialog.addButtonWithTitle('Cancel')
-        
-        dialog.alert().window().setInitialFirstResponder(dialog.viewAtIndex(1))
-        dialog.viewAtIndex(1).setNextKeyView(dialog.viewAtIndex(3))
-        dialog.viewAtIndex(3).setNextKeyView(dialog.viewAtIndex(5))
-        dialog.viewAtIndex(5).setNextKeyView(dialog.viewAtIndex(7))
-        dialog.viewAtIndex(7).setNextKeyView(dialog.viewAtIndex(1))
-        
-        return dialog.runModal()
-    }
-    var process = function() {
-        var width = dialog.viewAtIndex(1).stringValue()
-        var height = dialog.viewAtIndex(3).stringValue()
-        var max = dialog.viewAtIndex(5).stringValue()
-        var values = dialog.viewAtIndex(7).stringValue().split(',')
-        var count = values.length
-        
-        if (width.length() == 0 ||
-            height.length() == 0 ||
-            max.length() == 0 ||
-            values.length == 0) {
-            return
-        }
-        
-        var linePath = NSBezierPath.bezierPath()
-        for (var i = 0; i < count; i++) {
-            var x = width / (count - 1) * i
-            var y = (max - values[i]) / max * height
-            if (i === 0) {
-                linePath.moveToPoint(NSMakePoint(x,y))
-            } else {
-                linePath.lineToPoint(NSMakePoint(x,y))
-            }
-        }
-        var line = MSShapeGroup.shapeWithBezierPath(linePath)
-        var border = line.style().addStylePartOfType(1)
-        border.setColor(MSImmutableColor.colorWithIntegerRed_green_blue_alpha(0,0,0,51).newMutableCounterpart())
-        border.setThickness(2)
-        border.setPosition(0)
-        line.setName('Line')
-        context.selection[0].addLayers([line])
-    }
-    if (createUI() == 1000) {
-        process()
-    }
-}
+const showValueError = () => {
+  const alert = COSAlertWindow.alloc().init();
+  alert.setMessageText('Valid values are required.');
+  alert.runModal();
+};
 
-var makeFilledLineChart = function(context) {
-    var dialog
-    var createUI = function() {
-        dialog = COSAlertWindow.new()
-        dialog.setMessageText('Make Filled Line Chart')
-        dialog.addTextLabelWithValue('width')
-        dialog.addTextFieldWithValue('600')
-        dialog.addTextLabelWithValue('height')
-        dialog.addTextFieldWithValue('200')
-        dialog.addTextLabelWithValue('max value')
-        dialog.addTextFieldWithValue('100')
-        dialog.addTextLabelWithValue('values (divided by ",")')
-        dialog.addTextFieldWithValue('')
-        dialog.addButtonWithTitle('OK')
-        dialog.addButtonWithTitle('Cancel')
-        
-        dialog.alert().window().setInitialFirstResponder(dialog.viewAtIndex(1))
-        dialog.viewAtIndex(1).setNextKeyView(dialog.viewAtIndex(3))
-        dialog.viewAtIndex(3).setNextKeyView(dialog.viewAtIndex(5))
-        dialog.viewAtIndex(5).setNextKeyView(dialog.viewAtIndex(7))
-        dialog.viewAtIndex(7).setNextKeyView(dialog.viewAtIndex(1))
-        
-        return dialog.runModal()
+const style = {
+  shape: {
+    fills: [{
+      color: '#0006',
+    }],
+    borders: [],
+  },
+  line: {
+    fills: [],
+    borders: [{
+      color: '#000c',
+      position: Style.BorderPosition.Center,
+      thickness: 2,
+    }],
+    borderOptions: {
+      lineEnd: Style.LineEnd.Round,
+      lineJoin: Style.LineJoin.Round,
+    },
+  },
+};
+
+var makeBarChart = context => {
+  const selection = context.selection.firstObject() || context.document.currentPage();
+
+  const alert = COSAlertWindow.alloc().init();
+  alert.setMessageText('Make Bar Chart');
+  alert.addTextLabelWithValue('width');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(1).placeholderString = `${defaultValues.width}`;
+  alert.addTextLabelWithValue('height');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(3).placeholderString = `${defaultValues.height}`;
+  alert.addTextLabelWithValue('max value');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(5).placeholderString = 'Default to highest value';
+  alert.addTextLabelWithValue('values (divided by ,)');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(7).placeholderString = 'Required';
+  alert.addTextLabelWithValue('column width');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(9).placeholderString = 'Default to half width';
+  alert.addButtonWithTitle('OK');
+  alert.addButtonWithTitle('Cancel');
+  alert.alert().window().initialFirstResponder = alert.viewAtIndex(1);
+  alert.viewAtIndex(1).setNextKeyView(alert.viewAtIndex(3));
+  alert.viewAtIndex(3).setNextKeyView(alert.viewAtIndex(5));
+  alert.viewAtIndex(5).setNextKeyView(alert.viewAtIndex(7));
+  alert.viewAtIndex(7).setNextKeyView(alert.viewAtIndex(9));
+  alert.viewAtIndex(9).setNextKeyView(alert.viewAtIndex(1));
+
+  const process = () => {
+    const width = alert.viewAtIndex(1).doubleValue() || defaultValues.width;
+    const height = alert.viewAtIndex(3).doubleValue() || defaultValues.height;
+    let max = alert.viewAtIndex(5).doubleValue();
+    const values = alert.viewAtIndex(7).stringValue().split(',').map(v => parseFloat(v)).filter(v => !isNaN(v));
+    let columnWidth = alert.viewAtIndex(9).doubleValue();
+    const count = values.length;
+
+    if (values.length === 0) {
+      showValueError();
+      alert.buttons()[1].resignFirstResponder();
+      if (alert.runModal() === NSAlertFirstButtonReturn) {
+        process();
+      }
+      return;
     }
-    var process = function() {
-        var width = dialog.viewAtIndex(1).stringValue()
-        var height = dialog.viewAtIndex(3).stringValue()
-        var max = dialog.viewAtIndex(5).stringValue()
-        var values = dialog.viewAtIndex(7).stringValue().split(',')
-        var count = values.length
-        
-        if (width.length() == 0 ||
-            height.length() == 0 ||
-            max.length() == 0 ||
-            values.length == 0) {
-            return
-        }
-        
-        var linePath = NSBezierPath.bezierPath()
-        for (var i = 0; i < count; i++) {
-            var x = width / (count - 1) * i
-            var y = (max - values[i]) / max * height
-            if (i === 0) {
-                linePath.moveToPoint(NSMakePoint(x,y))
-            } else {
-                linePath.lineToPoint(NSMakePoint(x,y))
-            }
-        }
-        linePath.lineToPoint(NSMakePoint(width, height))
-        linePath.lineToPoint(NSMakePoint(0, height))
-        linePath.closePath()
-        var line = MSShapeGroup.shapeWithBezierPath(linePath)
-        var fill = line.style().addStylePartOfType(0)
-        fill.setColor(MSImmutableColor.colorWithIntegerRed_green_blue_alpha(0,0,0,51).newMutableCounterpart())
-        line.setName('Line')
-        context.selection[0].addLayers([line])
+    if (max === 0) {
+      max = Math.max(...values);
     }
-    if (createUI() == 1000) {
-        process()
+    if (columnWidth <= 0) {
+      columnWidth = width / (count * 2);
     }
-}
+
+    const firstShape = new Shape({
+      name: 'Chart',
+      parent: selection,
+      frame: new Rectangle(0, 0, width, height),
+      style: style.shape,
+    });
+    for (let i = count; i >= 0; i--) {
+      const x = width / count * i + (width / count - columnWidth) * 0.5;
+      const y = (max - values[i]) / max * height;
+      const w = columnWidth;
+      const h = height - y;
+
+      new ShapePath({
+        name: 'Bar',
+        parent: firstShape,
+        frame: new Rectangle(x, y, w, h),
+        style: style.shape,
+        shapeType: ShapePath.ShapeType.Rectangle,
+      });
+    };
+    firstShape.layers.shift(); // remove default path
+  };
+
+  if (alert.runModal() === NSAlertFirstButtonReturn) {
+    process();
+  }
+};
+
+var makeLineChart = context => {
+  const selection = context.selection.firstObject() || context.document.currentPage();
+
+  const alert = COSAlertWindow.alloc().init();
+  alert.setMessageText('Make Line Chart');
+  alert.addTextLabelWithValue('width');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(1).placeholderString = `${defaultValues.width}`;
+  alert.addTextLabelWithValue('height');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(3).placeholderString = `${defaultValues.height}`;
+  alert.addTextLabelWithValue('max value');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(5).placeholderString = 'Default to highest value';
+  alert.addTextLabelWithValue('values (divided by ,)');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(7).placeholderString = 'Required';
+  alert.addButtonWithTitle('OK');
+  alert.addButtonWithTitle('Cancel');
+  alert.alert().window().initialFirstResponder = alert.viewAtIndex(1);
+  alert.viewAtIndex(1).setNextKeyView(alert.viewAtIndex(3));
+  alert.viewAtIndex(3).setNextKeyView(alert.viewAtIndex(5));
+  alert.viewAtIndex(5).setNextKeyView(alert.viewAtIndex(7));
+  alert.viewAtIndex(7).setNextKeyView(alert.viewAtIndex(1));
+  
+  const process = () => {
+    const width = alert.viewAtIndex(1).doubleValue() || defaultValues.width;
+    const height = alert.viewAtIndex(3).doubleValue() || defaultValues.height;
+    let max = alert.viewAtIndex(5).doubleValue();
+    const values = alert.viewAtIndex(7).stringValue().split(',').map(v => parseFloat(v)).filter(v => !isNaN(v));
+    const count = values.length;
+
+    if (values.length === 0) {
+      showValueError();
+      alert.buttons()[1].resignFirstResponder();
+      if (alert.runModal() === NSAlertFirstButtonReturn) {
+        process();
+      }
+      return;
+    }
+    if (max === 0) {
+      max = Math.max(...values);
+    }
+
+    const points = values.map((v, i) => {
+      return {
+        point: {
+          x: width / (count - 1) * i / width,
+          y: (max - v) / max * height / height,
+        },
+      };
+    });
+    new ShapePath({
+      name: 'Line',
+      parent: selection,
+      frame: new Rectangle(0, 0, width, height),
+      style: style.line,
+      shapeType: ShapePath.ShapeType.Custom,
+      points,
+    });
+  };
+
+  if (alert.runModal() === NSAlertFirstButtonReturn) {
+    process();
+  }
+};
+
+var makeFilledLineChart = context => {
+  const selection = context.selection.firstObject() || context.document.currentPage();
+
+  const alert = COSAlertWindow.alloc().init();
+  alert.setMessageText('Make Filled Line Chart');
+  alert.addTextLabelWithValue('width');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(1).placeholderString = `${defaultValues.width}`;
+  alert.addTextLabelWithValue('height');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(3).placeholderString = `${defaultValues.height}`;
+  alert.addTextLabelWithValue('max value');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(5).placeholderString = 'Default to highest value';
+  alert.addTextLabelWithValue('values (divided by ,)');
+  alert.addTextFieldWithValue('');
+  alert.viewAtIndex(7).placeholderString = 'Required';
+  alert.addButtonWithTitle('OK');
+  alert.addButtonWithTitle('Cancel');
+  alert.alert().window().initialFirstResponder = alert.viewAtIndex(1);
+  alert.viewAtIndex(1).setNextKeyView(alert.viewAtIndex(3));
+  alert.viewAtIndex(3).setNextKeyView(alert.viewAtIndex(5));
+  alert.viewAtIndex(5).setNextKeyView(alert.viewAtIndex(7));
+  alert.viewAtIndex(7).setNextKeyView(alert.viewAtIndex(1));
+  
+  const process = () => {
+    const width = alert.viewAtIndex(1).doubleValue() || defaultValues.width;
+    const height = alert.viewAtIndex(3).doubleValue() || defaultValues.height;
+    let max = alert.viewAtIndex(5).doubleValue();
+    const values = alert.viewAtIndex(7).stringValue().split(',').map(v => parseFloat(v)).filter(v => !isNaN(v));
+    const count = values.length;
+
+    if (values.length === 0) {
+      showValueError();
+      alert.buttons()[1].resignFirstResponder();
+      if (alert.runModal() === NSAlertFirstButtonReturn) {
+        process();
+      }
+      return;
+    }
+    if (max === 0) {
+      max = Math.max(...values);
+    }
+
+    const points = values.map((v, i) => {
+      return {
+        point: {
+          x: width / (count - 1) * i / width,
+          y: (max - v) / max * height / height,
+        },
+      };
+    });
+    points.push(
+      { point: { x: 1, y: 1 } },
+      { point: { x: 0, y: 1 } }
+    );
+    new ShapePath({
+      name: 'Line',
+      parent: selection,
+      frame: new Rectangle(0, 0, width, height),
+      style: style.shape,
+      shapeType: ShapePath.ShapeType.Custom,
+      points,
+      closed: true,
+    });
+  };
+
+  if (alert.runModal() === NSAlertFirstButtonReturn) {
+    process();
+  }
+};
